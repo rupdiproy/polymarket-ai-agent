35e2f665e067ad9b169c49d08c757ee9a45c749c
Aziz Hakim <aziz@penstrike.io>
2026-02-22 19:39:13 -0500

    Silence turbopack warning strictly with empty object

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: any = {
   typescript: { ignoreBuildErrors: true },
-  turbopack: {
-    resolveAlias: {
-      "@react-native-async-storage/async-storage": "./async-storage-mock.js",
-    },
-  },
+  turbopack: {},
   webpack: (config: any) => {
     if (!config.resolve) config.resolve = {};
     config.resolve.alias = {
